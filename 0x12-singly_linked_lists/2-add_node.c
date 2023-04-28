ce328f04dd7e27d436929035f57541f356e4f932
njine <brikab@gmail.com>
2023-04-29 00:17:00 +0300

    singly1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,23 +11,26 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    if (!head || !str)
-        return NULL;
+    list_t *new_node;
 
-    list_t *new_node = calloc(1, sizeof(list_t));
+    if (!head || !str)
+        return (NULL);
+
+    new_node = calloc(1, sizeof(list_t));
     if (!new_node)
-        return NULL;
+        return (NULL);
 
     new_node->str = strdup(str);
     if (!new_node->str)
     {
         free(new_node);
-        return NULL;
+        return (NULL);
     }
 
     new_node->len = strlen(str);
     new_node->next = *head;
     *head = new_node;
 
-    return new_node;
+    return (new_node);
 }
+
