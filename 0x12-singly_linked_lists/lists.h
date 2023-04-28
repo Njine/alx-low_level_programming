6acc61149f35de0aa0c3facbf1a59af263f1f6ab
njine <brikab@gmail.com>
2023-04-28 23:31:40 +0300

    add node6

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,9 +3,9 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * @str: string - (malloc'ed string) 
  * @next: points to the next node
+ * @len: length of the string
  *
  * Description: singly linked list node structure
  * for the alx project
@@ -18,9 +18,11 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+size_t list_len(const list_t *h);
+
+
 
 #endif
