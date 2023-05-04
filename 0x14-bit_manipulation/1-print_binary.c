06fd50d663ddb7c92ef8256fba1edf53b8f0ea2b
njine <brikab@gmail.com>
2023-05-05 00:54:12 +0300

    0-4

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,28 +2,34 @@
 #include <stdio.h>
 
 /**
- * print_binary - print binary representation of a number
- * @n: decimal number to print as binary
- */
+* print_binary - print binary representation of a number
+* @n: decimal number to print as binary
+*/
 void print_binary(unsigned long int n)
 {
-	unsigned long int temp;
-	int shifts;
+unsigned long int temp;
+int num_bits = 0;
 
-	if (n == 0)
-	{
-		printf("0");
-		return;
-	}
-
-	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-		;
-
-	for (; shifts >= 0; shifts--)
-	{
-		if ((n >> shifts) & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+if (n == 0)
+{
+putchar('0');
+return;
 }
+
+temp = n;
+while (temp > 0)
+{
+num_bits++;
+temp >>= 1;
+}
+
+while (num_bits > 0)
+{
+if ((n >> (num_bits - 1)) & 1)
+putchar('1');
+else
+putchar('0');
+num_bits--;
+}
+}
+
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,20 +2,20 @@
 
 /**
  * set_bit - sets value of a bit to 1 at a given index
- * @n: decimal number passed by pointer
+ * @n: pointer to decimal number
  * @index: index position to change, starting from 0
+ *
  * Return: 1 if it worked, -1 if error
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int p;
+	if (index > 63)
+	return (-1);
 
-	if (index > 64)
-		return (-1);
+	unsigned long int bit_value = 1UL << index;
 
-	for (p = 1; index > 0; index--, p *= 2)
-		;
-	*n += p;
+	*n |= bit_value;
 
 	return (1);
 }
+
