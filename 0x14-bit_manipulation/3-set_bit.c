962936ec9417b29e1368e624b0b0342075d8176c
njine <brikab@gmail.com>
2023-05-05 02:04:59 +0300

    0-4-4

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,13 +10,13 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
-	return (-1);
+		return (-1);
 
-	unsigned long int mask = 1UL << index;
-	*n |= mask;
+	*n |= (1UL << index);
 
 	return (1);
 }
+
 
 
 
