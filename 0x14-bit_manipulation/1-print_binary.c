6c579dba8ee3179321cb78b3e74a4db493131cde
njine <brikab@gmail.com>
2023-07-07 01:50:42 +0300

    back

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_binary - print binary image of number
+* @n: decimal no to print as binary
+*/
+void print_binary(unsigned long int n)
+{
+unsigned long int var;
+int numbit = 0;
+
+if (n == 0)
+{
+putchar('0');
+return;
+}
+
+var = n;
+while (var > 0)
+{
+numbit++;
+var >>= 1;
+}
+
+while (numbit > 0)
+{
+if ((n >> (numbit - 1)) & 1)
+putchar('1');
+else
+putchar('0');
+numbit--;
+}
+}
+
