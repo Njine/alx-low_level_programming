9f3dd213a617daaafa1cec59a387df731649a1e0
njine <brikab@gmail.com>
2023-07-11 01:25:56 +0300

    21

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -25,17 +25,19 @@ void close_elf(int elf);
  */
 void check_elf(unsigned char *e_ident)
 {
-    int index;
-    unsigned char expected_values[] = {0x7F, 'E', 'L', 'F'};
+	int index;
 
-    for (index = 0; index < 4; index++)
-    {
-        if (e_ident[index] != expected_values[index])
-        {
-            dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-            exit(98);
-        }
-    }
+	for (index = 0; index < 4; index++)
+	{
+		if (e_ident[index] != 127 &&
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
 }
 
 /**
@@ -46,19 +48,19 @@ void check_elf(unsigned char *e_ident)
  */
 void print_magic(unsigned char *e_ident)
 {
-    int index;
+	int index;
 
-    printf("  Magic:   ");
+	printf("  Magic:   ");
 
-    for (index = 0; index < EI_NIDENT; index++)
-    {
-        printf("%02x", e_ident[index]);
+	for (index = 0; index < EI_NIDENT; index++)
+	{
+		printf("%02x", e_ident[index]);
 
-        if (index != EI_NIDENT - 1)
-            printf(" ");
-    }
-    
-    printf("\n");
+		if (index == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
 }
 
 /**
@@ -67,26 +69,23 @@ void print_magic(unsigned char *e_ident)
  */
 void print_class(unsigned char *e_ident)
 {
-    printf("  Class:                             ");
+	printf("  Class:                             ");
 
-    if (e_ident[EI_CLASS] == ELFCLASSNONE)
-    {
-        printf("none\n");
-    }
-    else if (e_ident[EI_CLASS] == ELFCLASS32)
-    {
-        printf("ELF32\n");
-    }
-    else if (e_ident[EI_CLASS] == ELFCLASS64)
-    {
-        printf("ELF64\n");
-    }
-    else
-    {
-        printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-    }
+	switch (e_ident[EI_CLASS])
+	{
+	case ELFCLASSNONE:
+		printf("none\n");
+		break;
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
 }
-
 
 /**
  * print_data - Prints the data of an ELF header.
@@ -94,29 +93,23 @@ void print_class(unsigned char *e_ident)
  */
 void print_data(unsigned char *e_ident)
 {
-    printf("  Data:                              ");
+	printf("  Data:                              ");
 
-    const char *data_type;
-
-    switch (e_ident[EI_DATA])
-    {
-        case ELFDATANONE:
-            data_type = "none";
-            break;
-        case ELFDATA2LSB:
-            data_type = "2's complement, little endian";
-            break;
-        case ELFDATA2MSB:
-            data_type = "2's complement, big endian";
-            break;
-        default:
-            data_type = "<unknown>";
-            break;
-    }
-
-    printf("%s\n", data_type);
+	switch (e_ident[EI_DATA])
+	{
+	case ELFDATANONE:
+		printf("none\n");
+		break;
+	case ELFDATA2LSB:
+		printf("2's complement, little endian\n");
+		break;
+	case ELFDATA2MSB:
+		printf("2's complement, big endian\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
 }
-
 
 /**
  * print_version - Prints the version of an ELF header.
